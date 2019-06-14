9817b091095f0102febbcd506a94d02137ad2b9e
Thiago Cezario <thiagocezario2@gmail.com>
2019-06-14 03:55:05 -0300

    Configure screens and finish result screen

--- app/src/main/java/com/example/matematicadivertida/Result.java
+++ app/src/main/java/com/example/matematicadivertida/Result.java
@@ -1,13 +1,36 @@
 package com.example.matematicadivertida;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Result extends AppCompatActivity {
+    private TextView score;
+    private Button goBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        Intent intent = getIntent();
+        String totalScore = intent.getStringExtra("score");
+        
+        score = findViewById(R.id.score);
+        goBack = findViewById(R.id.goBack);
+
+        goBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Result.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        score.setText(totalScore);
     }
 }
